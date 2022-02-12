037152c5b8b5534e8012036e475b5bb7eef22eef
jicu mollick <jikumollick82@gmail.com>
2022-02-12 22:28:35 +0600

    all done

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 
 let count = 0;
 
-document.getElementById('plus').addEventListener('click', function(e){
+document.getElementById('add-btn').addEventListener('click',function(){
     count++;
-    document.getElementById('count').innerText = count;
-})
+    let inputValue = document.getElementById('user-input').value;
 
-document.getElementById('minus').addEventListener('click',function(e){
+if(inputValue == ""){
+    alert('please enter a value');
+}else {
+
+    const mainContainer = document.getElementById('table-container');
+
+    const tableContainer = document.createElement('tr');
+    tableContainer.innerHTML = `<th>${count}</th>
+    <td>${inputValue}</td>
+    <td><button class='btn btn-danger delete-btn'>Delete</button>
+    <button class='btn btn-success done-btn'>Done</button></td>`;
+
+    mainContainer.appendChild(tableContainer);
     
-    count--;
-    if(count < 0){
-        count = 0;
-    }
-    document.getElementById('count').innerText = count;   
+}
+
+document.getElementById('user-input').value = '';
+
+const deleteButton = document.getElementsByClassName('delete-btn');
+const doneButton = document.getElementsByClassName('done-btn');
+
+for(let button of deleteButton){
+    button.addEventListener('click',function(e){
+        e.target.parentNode.parentNode.style.display = 'none';
+    })
+}
+
+for(let button of doneButton){
+    button.addEventListener('click',function(e){
+        e.target.parentNode.parentNode.style.textDecoration = 'line-through';
+    })
+}
+
+
 })
 
+
+
+ function deleteAll(){
+    const mainContainer = document.getElementById('table-container');
+    mainContainer.textContent = '';
+ }
+
+ 
